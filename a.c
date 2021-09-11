fd7125c8ad159612bda83fe9c54e84823aea62b1
mohamedmostafa000 <mm3233166@gmail.com>
2021-09-11 13:11:57 -0700

    micro

---
+++ a.c
@@ -0,0 +1,16 @@
+/*
+ * File:   a.c
+ * Author: Hello
+ *
+ * Created on September 11, 2021, 1:08 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+        
+    }
+}
